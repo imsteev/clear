ec707f09a3a8b097655c1b5dbd35f4649ce121ef
Stephen Chung <spchung@andrew.cmu.edu>
2018-01-29 15:43:31 -0500

    basic datastore

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect,url_for
+from models.Shipment import Shipment
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', shipments=list(Shipment.query()))
 
-def register():
-    pass
+@app.route('/shipments', methods=['GET'])
+def shipments():
+    return render_template('index.html', shipments=list(Shipment.query()))
 
 @app.route('/track', methods=['POST'])
 def track():
     carrier_name = request.form['carrier']
     tracking_id = request.form['trackingid']
+    shipment_name = request.form['shipmentname']
 
-    print carrier_name, tracking_id
+    shipment = Shipment(name=shipment_name, carrier=carrier_name, tracking_id=tracking_id)
+    shipment.put()
     
-    return render_template('index.html')
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
+    return redirect(url_for('index'))
--- models/Shipment.py
+++ models/Shipment.py
@@ -1,8 +1,14 @@
-class Shipment(object):
-    def __init__(self, trackingid, carrier, description=None):
-        self.trackingid = trackingid
-        self.carrier = carrier
-        self.description = description
-    
-    def getTrackingId(self):
-        return self.trackingid
+from google.appengine.ext import ndb
+
+class Shipment(ndb.Model):
+    name = ndb.StringProperty()
+    tracking_id = ndb.StringProperty()
+    carrier = ndb.StringProperty()
+    #description = ndb.StringProperty()
+
+    def to_dict(self):
+        return {
+            "tracking_id": str(self.tracking_id),
+            "carrier": str(self.carrier),
+            "name": str(self.name)
+        }
